89581d55b796ce5c1a5ac63096700d98f5977f03
Losilof39 <lmarosevic7@gmail.com>
2026-06-05 10:43:32 +0200

    add: convert_g1_dataset.sh

---
+++ convert_g1_dataset.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python scripts/convert_unitree_to_zarr.py \
+      --input_dir /home/luke/MSMD/repos/unitree/xr_teleoperate/teleop/utils/data/empty_bucket_v2 \
+      --output_zarr Improved-3D-Diffusion-Policy/data/g1_empty_bucket_v2 \
+      --fx 607.7126 --fy 607.5232 --cx 319.4251 --cy 243.5345 \
+      --z_near 0.1 --z_far 1
